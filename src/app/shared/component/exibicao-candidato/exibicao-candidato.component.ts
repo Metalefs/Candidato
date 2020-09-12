39cbb025fdb873113ebf9578727c48e94e969b0e
Jackson <jack-ten@hotmail.com>
2020-09-12 20:41:59 -0300

    alteracao canvas desktop

--- src/app/shared/component/exibicao-candidato/exibicao-candidato.component.ts
+++ src/app/shared/component/exibicao-candidato/exibicao-candidato.component.ts
@@ -6,6 +6,7 @@ import { Candidato } from 'src/app/data/schema/domain';
 import { TipoImagem } from 'src/app/data/schema/TipoImagem';
 import { fade, slide } from 'src/app/animations';
 import { RedeSocial } from 'src/app/data/schema/RedeSocial';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-exibicao-candidato',
@@ -126,7 +127,7 @@ export class ExibicaoCandidatoComponent implements OnInit {
                 if (this.x < 0 || this.x > c.width) this.vx *= -this.vx;
                 if (this.y < 0 || this.y > c.height) this.vy *= -this.vy;
 
-                this.alpha -= 0.015;
+                this.alpha -= 0.0015;
                 this.r += growth;
                 this.draw();
             }
@@ -148,10 +149,11 @@ export class ExibicaoCandidatoComponent implements OnInit {
         window.addEventListener('click', function () {
             colorArray.push(colorArray.shift());
             colors = colorArray[0];
-            let vx = 5;
-            if(mouse.x > c.width - 1500)
-              vx *= -1;
-            circles.push(new Circle(mouse.x, mouse.y, vx, 5, 2, 5,c));
+            var vx = (Math.random() - 0.5) * 10 + (Math.random() < 0.5 ? -2 : 12);
+            var vy = (Math.random() - 0.5) * 10 + (Math.random() < 0.5 ? -2 : 12);
+            let r = 1;
+            let g = 2;
+            circles.push(new Circle(mouse.x, mouse.y, vx, vy, r, g, c));
         });
 
         // ensure canvas is always full size of browser window
@@ -170,13 +172,23 @@ export class ExibicaoCandidatoComponent implements OnInit {
 
             var vx = (Math.random() - 0.5) * 5 + (Math.random() < 0.5 ? -2 : 2);
             var vy = (Math.random() - 0.5) * 5 + (Math.random() < 0.5 ? -2 : 2);
-            var r = Math.random() * 20 + 30;
-            //circles.push(new Circle(mouse.x, mouse.y, vx, vy, r, -0.5));
+
+
+            if (x < 0 || x > c.width) vx = -vx;
+            if (y < 0 || y > c.height) vy = -vy;
+
+            // var r = Math.random() * 20 + 30;
+            // if(r > 0)
+            // circles.push(new Circle(mouse.x, mouse.y, vx, vy, r, 0.5, c));
 
             for (let i = 0; i < circles.length; ++i) {
                 circles[i].update();
                 // remove the circle if it is transparent or too small
-                if (circles[i].alpha < 0 || circles[i].r < 3 || circles.length > 10 ) {
+                if (
+                    circles[i].alpha < 0 ||
+                    circles[i].r < 3 ||
+                    circles.length > 100
+                   ) {
                     circles.splice(i, 1);
                 }
             }
@@ -211,76 +223,76 @@ export class ExibicaoCandidatoComponent implements OnInit {
     // Push stars to array
 
     for (var i = 0; i < x; i++) {
-    stars.push({
-    x: Math.random() * canvas.width,
-    y: Math.random() * canvas.height,
-    radius: Math.random() * 1 + 1,
-    vx: Math.floor(Math.random() * 50) - 25,
-    vy: Math.floor(Math.random() * 50) - 25
-    });
+      stars.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        radius: Math.random() * 1 + 1,
+        vx: Math.floor(Math.random() * 50) - 25,
+        vy: Math.floor(Math.random() * 50) - 25
+      });
     }
 
     // Draw the scene
 
     function draw() {
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+      ctx.clearRect(0,0,canvas.width,canvas.height);
 
-    ctx.globalCompositeOperation = "lighter";
+      ctx.globalCompositeOperation = "lighter";
 
-    for (var i = 0, x = stars.length; i < x; i++) {
-    var s = stars[i];
+      for (var i = 0, x = stars.length; i < x; i++) {
+        var s = stars[i];
 
-    ctx.fillStyle = "rgba(255,255,255,.5)";
-    ctx.beginPath();
-    ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.fillStyle = 'black';
-    ctx.stroke();
-    }
-
-    ctx.beginPath();
-    for (var i = 0, x = stars.length; i < x; i++) {
-    var starI = stars[i];
-    ctx.moveTo(starI.x,starI.y); 
-    if(distance(mouse, starI) < 150) ctx.lineTo(mouse.x, mouse.y);
-    for (var j = 0, x = stars.length; j < x; j++) {
-      var starII = stars[j];
-      if(distance(starI, starII) < 150) {
-        //ctx.globalAlpha = (1 / 150 * distance(starI, starII).toFixed(1));
-        ctx.lineTo(starII.x,starII.y); 
+        ctx.fillStyle = "rgba(255,255,255,.5)";
+        ctx.beginPath();
+        ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.fillStyle = 'black';
+        ctx.stroke();
       }
-    }
-    }
-    ctx.lineWidth = 0.05;
-    ctx.strokeStyle = 'white';
-    ctx.stroke();
+
+      ctx.beginPath();
+      for (var i = 0, x = stars.length; i < x; i++) {
+        var starI = stars[i];
+        ctx.moveTo(starI.x,starI.y); 
+        if(distance(mouse, starI) < 150) ctx.lineTo(mouse.x, mouse.y);
+        for (var j = 0, x = stars.length; j < x; j++) {
+          var starII = stars[j];
+          if(distance(starI, starII) < 150) {
+            //ctx.globalAlpha = (1 / 150 * distance(starI, starII).toFixed(1));
+            ctx.lineTo(starII.x,starII.y); 
+          }
+        }
+      }
+      ctx.lineWidth = 0.05;
+      ctx.strokeStyle = 'white';
+      ctx.stroke();
     }
 
     function distance( point1, point2 ){
-    var xs = 0;
-    var ys = 0;
+      var xs = 0;
+      var ys = 0;
 
-    xs = point2.x - point1.x;
-    xs = xs * xs;
+      xs = point2.x - point1.x;
+      xs = xs * xs;
 
-    ys = point2.y - point1.y;
-    ys = ys * ys;
+      ys = point2.y - point1.y;
+      ys = ys * ys;
 
-    return Math.sqrt( xs + ys );
+      return Math.sqrt( xs + ys );
     }
 
     // Update star locations
 
     function update() {
-    for (var i = 0, x = stars.length; i < x; i++) {
-    var s = stars[i];
+      for (var i = 0, x = stars.length; i < x; i++) {
+        var s = stars[i];
 
-    s.x += s.vx / FPS;
-    s.y += s.vy / FPS;
+        s.x += s.vx / FPS;
+        s.y += s.vy / FPS;
 
-    if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
-    if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
-    }
+        if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
+        if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
+      }
     }
 
     canvas.addEventListener('mousemove', function(e){
@@ -291,9 +303,9 @@ export class ExibicaoCandidatoComponent implements OnInit {
     // Update and draw
 
     function tick() {
-    draw();
-    update();
-    requestAnimationFrame(tick);
+      draw();
+      update();
+      requestAnimationFrame(tick);
     }
 
     tick();
